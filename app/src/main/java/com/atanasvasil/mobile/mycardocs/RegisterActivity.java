b58cab40a0709e82cc48d011b237da4d7bb63de8
Vasil903 <vsk123@abv.bg>
2021-02-12 08:42:26 +0200

    Update MyCarDocs - RegisterActivity

--- app/src/main/java/com/atanasvasil/mobile/mycardocs/RegisterActivity.java
+++ app/src/main/java/com/atanasvasil/mobile/mycardocs/RegisterActivity.java
@@ -1,10 +1,15 @@
 package com.atanasvasil.mobile.mycardocs;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.atanasvasil.mobile.mycardocs.responses.users.User;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -34,7 +39,49 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
         registerBtn.setOnClickListener(v -> {
-            // Registration button
+            if (v.getId() == R.id.registerBtn) {
+                if (registerEmailЕТ.getText().length() > 0
+                        && registerPasswordЕТ.getText().length() > 0
+                        && registerPasswordЕТ.getText().toString().equals(registerConfirmPasswordЕТ.getText().toString())) {
+
+                    String password = registerPasswordЕТ.getText().toString();
+                    String firstName = registerFirstNameЕТ.getText().toString();
+                    String lastName = registerLastNameЕT.getText().toString();
+
+                    User user = new User();
+                    user.setPassword(password);
+                    user.setFirstName(firstName);
+                    user.setLastName(lastName);
+
+                    new RegisterAsyncTask(user).execute();
+
+                } else {
+                    Toast.makeText(RegisterActivity.this,
+                            "please fill in all fields!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
         });
+
+        private class RegisterAsyncTask extends AsyncTask<Void, Void, Void> {
+
+            User user;
+            boolean isSuccess = false;
+
+            ProgressDialog dialog;
+
+            RegisterAsyncTask(User user) {
+                this.user = user;
+                dialog = new ProgressDialog(RegisterActivity.this);
+            }
+
+            @Override
+            protected void onPreExecute() {
+                dialog.setTitle("Registration in progress...");
+                dialog.show();
+            }
+            // Registration button
+
+        }
     }
 }
