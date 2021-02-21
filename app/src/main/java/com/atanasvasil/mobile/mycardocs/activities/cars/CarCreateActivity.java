a126c723482224a40d4a97b5adb56cff4d774be1
Atanas Arshinkov <a.arshinkov97@gmail.com>
2021-02-21 17:23:59 +0200

    Added car create back button and title

--- app/src/main/java/com/atanasvasil/mobile/mycardocs/activities/cars/CarCreateActivity.java
+++ app/src/main/java/com/atanasvasil/mobile/mycardocs/activities/cars/CarCreateActivity.java
@@ -1,8 +1,10 @@
 package com.atanasvasil.mobile.mycardocs.activities.cars;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 import com.atanasvasil.mobile.mycardocs.R;
@@ -21,11 +23,19 @@ public class CarCreateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_create);
 
+        getSupportActionBar().setTitle("Create a car");
+
         carCreateBrandET = findViewById(R.id.carCreateBrandET);
         carCreateModelET = findViewById(R.id.carCreateModelET);
         carCreateColorET = findViewById(R.id.carCreateColorET);
         carCreateYearET = findViewById(R.id.carCreateYearET);
         carCreateLicensePlateET = findViewById(R.id.carCreateLicensePlateET);
         carCreateAliasET = findViewById(R.id.carCreateAliasET);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        onBackPressed();
+        return true;
     }
 }
